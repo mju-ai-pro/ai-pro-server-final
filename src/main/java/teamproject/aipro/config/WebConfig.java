718dbd1f45fbfe20bdccd8f2c83d3bb0817fe785
choejonghyeon <choijonghyen@gmail.com>
2024-12-12 20:16:29 +0900

    refactor: config 수정

--- src/main/java/teamproject/aipro/config/WebConfig.java
+++ src/main/java/teamproject/aipro/config/WebConfig.java
@@ -1,18 +1,26 @@
 package teamproject.aipro.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	@Override
-	public void addCorsMappings(@NonNull CorsRegistry registry) {
+	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns("*")
-			.allowedMethods("*")
-			.allowedHeaders("*")
+			.allowedOrigins("https://www.mjuaipro.site", "https://ai-pro-fe.vercel.app")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("Content-Type", "Authorization", "Accept",
+				"Sec-CH-UA", "Sec-CH-UA-Mobile", "Sec-CH-UA-Platform")
+			.exposedHeaders("Authorization")
 			.allowCredentials(true);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
